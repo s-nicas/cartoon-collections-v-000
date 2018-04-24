fb375e016440c328d83f2368ca927fe34ae02520
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-24 21:50:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,6 @@ end
 def summon_captain_planet(planeteer_calls)
   updated_planeteer_calls =[]
    planeteer_calls.each do |calls|
-     
      updated_planeteer_calls.push("#{calls.capitalize}!")
    end 
    return updated_planeteer_calls
